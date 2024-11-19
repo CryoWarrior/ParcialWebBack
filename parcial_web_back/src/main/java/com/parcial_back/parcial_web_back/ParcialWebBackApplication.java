5a3993dc53968dd2c9faab252d296963bebd908c
CryoWarrior <diegoalejandrojararojas@gmail.com>
2024-11-19 16:12:59 -0500

    Creacion proyecto

---
+++ parcial_web_back/src/main/java/com/parcial_back/parcial_web_back/ParcialWebBackApplication.java
@@ -0,0 +1,13 @@
+package com.parcial_back.parcial_web_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialWebBackApplication.class, args);
+	}
+
+}
